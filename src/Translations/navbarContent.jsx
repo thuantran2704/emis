300565bbff6672deb5479b344f26c40104968b5d
Thuan Tran <tqt@usf.edu>
2025-11-09 13:22:50 -0500

    update name

--- src/Translations/navbarContent.jsx
+++ src/Translations/navbarContent.jsx
@@ -1,41 +1,46 @@
 const navbarContent = {
-    english: {
-      home: "Home",
-      about: "About",
-      services: "Services",
-      contact: "Contact",
-    },
-    vietnamese: {
-      home: "Trang Chủ",
-      about: "Về Chúng Tôi",
-      services: "Dịch Vụ",
-      contact: "Liên Hệ",
-    },
-    french: {
-      home: "Accueil",
-      about: "À Propos",
-      services: "Services",
-      contact: "Contact",
-    },
-    simplified: {
-      home: "首页",
-      about: "关于我们",
-      services: "服务",
-      contact: "联系",
-    },
-    traditional: {
-      home: "首頁",
-      about: "關於我們",
-      services: "服務",
-      contact: "聯絡我們",
-    },
-    korean: {
-      home: "홈",
-      about: "소개",
-      services: "서비스",
-      contact: "연락처",
-    },
-  };
-  
-  export default navbarContent;
-  
+  english: {
+    home: "Home",
+    about: "About",
+    services: "Services",
+    contact: "Contact",
+    equipment: "Equipment",
+  },
+  vietnamese: {
+    home: "Trang Chủ",
+    about: "Về Chúng Tôi",
+    services: "Dịch Vụ",
+    contact: "Liên Hệ",
+    equipment: "Trang Thiết Bị",
+  },
+  french: {
+    home: "Accueil",
+    about: "À Propos",
+    services: "Services",
+    contact: "Contact",
+    equipment: "Équipements",
+  },
+  simplified: {
+    home: "首页",
+    about: "关于我们",
+    services: "服务",
+    contact: "联系",
+    equipment: "设备",
+  },
+  traditional: {
+    home: "首頁",
+    about: "關於我們",
+    services: "服務",
+    contact: "聯絡我們",
+    equipment: "設備",
+  },
+  korean: {
+    home: "홈",
+    about: "소개",
+    services: "서비스",
+    contact: "연락처",
+    equipment: "장비",
+  },
+};
+
+export default navbarContent;
