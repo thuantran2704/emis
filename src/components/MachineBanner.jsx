ee5970b0eacff4e317965f4efe846d50c4a11a94
Thuần Trần <thuantran2704@gmail.com>
2025-11-10 23:04:57 -0500

    update price chart fix

--- src/components/MachineBanner.jsx
+++ src/components/MachineBanner.jsx
@@ -15,7 +15,7 @@ export default function MachineBanner({ language }) {
 
   const machines = [
     { key: "vatech", img: vatechImg },
-    { key: "kehong", img: kehongImg },
+    { key: "kehong", img: kehongI                     mg },
     { key: "hyperlight", img: hyperlightImg },
     { key: "uvStorage", img: uvStorageImg },
     { key: "hyundai", img: hyundaiImg },
