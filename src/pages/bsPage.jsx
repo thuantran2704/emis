2ab2b3111187487bf461de746c5c4e05f6c0190d
Thuần Trần <thuantran2704@gmail.com>
2025-11-24 00:47:08 -0500

    pics

--- src/pages/bsPage.jsx
+++ src/pages/bsPage.jsx
@@ -7,10 +7,14 @@ import bs2 from '../pics/bs/bs2.png';
 import bs3 from '../pics/bs/bs3.png';
 import bs4 from '../pics/bs/bs4.png';
 import sonPapers from '../pics/bs/sonPapers.jpg';
+import { useMemo } from 'react';
 
 export default function SonPage() {
   const language = useSelector((state) => state.language);
-  const content = language === 'vietnamese' ? sonContent.vietnamese : sonContent.english;
+  const content = useMemo(() => {
+    return language === 'vietnamese' ? sonContent.vietnamese : sonContent.english;
+  }, [language]);
+
   const images = [bs0, bs1, bs2, bs3, bs4];
 
   return (
